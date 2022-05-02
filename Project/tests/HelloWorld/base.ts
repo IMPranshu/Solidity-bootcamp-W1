f81320700feaae0bec2d3b743cdfa6e33eaeeb1c
Matheus Pagani <matheusdaros77@hotmail.com>
2022-05-02 20:57:21 -0300

    Solution

---
+++ Project/tests/HelloWorld/base.ts
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+// eslint-disable-next-line node/no-missing-import
+import { HelloWorld } from "../../typechain";
+
+describe("HelloWorld", function () {
+  let helloWorldContract: HelloWorld;
+
+  this.beforeEach(async function () {
+    const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
+    helloWorldContract = await helloWorldFactory.deploy();
+    await helloWorldContract.deployed();
+  });
+
+  it("Should give a Hello World", async function () {
+    expect(await helloWorldContract.helloWorld()).to.equal("Hello World");
+  });
+
+  it("Should set owner to deployer account", async function () {
+    const accounts = await ethers.getSigners();
+    expect(await helloWorldContract.owner()).to.equal(accounts[0].address);
+  });
+
+  it("Should not allow anyone other than owner to call transferOwnership", async function () {
+    const accounts = await ethers.getSigners();
+    await expect(
+      helloWorldContract
+        .connect(accounts[1])
+        .transferOwnership(accounts[1].address)
+    ).to.be.revertedWith("Caller is not the owner");
+  });
+
+  it("Should change text correctly", async function () {
+    // TODO
+    throw Error("Not implemented");
+  });
+
+  it("Should not allow anyone other than owner to change text", async function () {
+    // TODO
+    throw Error("Not implemented");
+  });
+});
